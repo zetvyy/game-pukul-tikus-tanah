defb753d5b17c072e698bb4533172a251c93d83d
zetvyy <zetvyy@gmail.com>
2021-09-20 19:58:48 +0700

    menambahkan function untuk menampilkan tikus

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
 const tanah = document.querySelectorAll(".tanah");
 const tikus = document.querySelectorAll(".tikus");
 
+let tanahSebelumnya;
+let selesai = false;
+
 function randomTanah(tanah) {
-  const tanahRandom = Math.floor(Math.random() * tanah.length);
-  return tanahRandom;
+  const t = Math.floor(Math.random() * tanah.length);
+  const tRandom = tanah[t];
+  if (tRandom == tanahSebelumnya) {
+    randomTanah(tanah);
+  }
+  tanahSebelumnya = tRandom;
+  return tRandom;
 }
 
-function munculkanTikus(tanah) {
-  tanah[0].classList.add("muncul");
+function randomWaktu(min, max) {
+  return Math.random() * (max - min + min);
+}
+
+function munculkanTikus() {
+  const tRandom = randomTanah(tanah);
+  const wRandom = randomWaktu(300, 950);
+  tRandom.classList.add("muncul");
+
+  setTimeout(() => {
+    tRandom.classList.remove("muncul");
+    if (!selesai) {
+      munculkanTikus();
+    }
+  }, wRandom);
+}
+
+function mulai() {
+  munculkanTikus();
+  setTimeout(() => {
+    selesai = true;
+  }, 10000);
 }
