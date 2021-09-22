337a82d5fa21ba18e68455466d657925f2ed0d72
zetvyy <zetvyy@gmail.com>
2021-09-22 10:09:29 +0700

    membuat function ketika pukul tikus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const tanah = document.querySelectorAll(".tanah");
 const tikus = document.querySelectorAll(".tikus");
+const papanSkor = document.querySelector(".papan-skor");
 
 let tanahSebelumnya;
-let selesai = false;
+let selesai;
+let skor;
 
 function randomTanah(tanah) {
   const t = Math.floor(Math.random() * tanah.length);
@@ -32,8 +34,21 @@ function munculkanTikus() {
 }
 
 function mulai() {
+  selesai = false;
+  skor = 0;
+  papanSkor.textContent = 0;
   munculkanTikus();
   setTimeout(() => {
     selesai = true;
   }, 10000);
 }
+
+function pukul() {
+  skor++;
+  this.parentNode.className = "tanah";
+  papanSkor.textContent = skor;
+}
+
+tikus.forEach(t => {
+  t.addEventListener("click", pukul);
+});
